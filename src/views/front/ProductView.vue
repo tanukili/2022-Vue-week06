efc95d352cbd2166bf03af7280e36f265dd262c9
tanukile <backpacker320@gmail.com>
2023-03-12 03:59:28 +0800

    產品相關

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
     前台單一商品
+    <h2>{{ product.title }}</h2>
+    <img :src="product.imageUrl" class="img-fluid" alt="">
   </div>
 </template>
+
+<script>
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      product: {}
+    }
+  },
+  methods: {
+    getProduct () {
+      const { id } = this.$route.params
+      this.$http.get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/product/${id}`)
+        .then((res) => {
+          this.product = res.data.product
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+        })
+    }
+  },
+  mounted () {
+    this.getProduct()
+  }
+}
+</script>
--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -2,4 +2,47 @@
   <div>
     前台產品列表
   </div>
+  <table class="table">
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+      <td>{{ product.title }}</td>
+      <td><img :src="product.imageUrl" width="150" alt=""></td>
+      <td>
+        <RouterLink :to="`/product/${product.id}`">商品細節</RouterLink>
+      </td>
+    </tr>
+    </tbody>
+  </table>
 </template>
+
+<script>
+import { RouterLink } from 'vue-router'
+// 取得環境變數
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  methods: {
+    getProdcuts () {
+      // 插件方式：取得遠端資料
+      this.$http.get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products`)
+        .then((res) => {
+          this.products = res.data.products
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+        })
+    }
+  },
+  mounted () {
+    this.getProdcuts()
+  },
+  components: {
+    RouterLink
+  }
+}
+</script>
